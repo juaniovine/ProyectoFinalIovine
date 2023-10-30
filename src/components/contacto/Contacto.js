1c0acb4d76914799a0bf065e657caadba2b4aa58
juaniovine <juaniovine@gmail.com>
2023-10-30 15:07:36 -0300

    added base contact form

--- src/components/contacto/Contacto.js
+++ src/components/contacto/Contacto.js
@@ -2,8 +2,31 @@ import React, {useState} from 'react'
 
 
 export default function Contacto() {
-  
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes manejar la lógica de envío del formulario
+    console.log(`Nombre: ${nombre}, Apellido: ${apellido}, Email: ${email}`);
+  };
+
   return (
-    <div></div>
-  )
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nombre:
+        <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+      </label>
+      <label>
+        Apellido:
+        <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <input type="submit" value="Enviar" />
+    </form>
+  );
 }
